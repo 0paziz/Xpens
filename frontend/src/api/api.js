5a8de503825871fad38113b45e5a85880ecac707
0paziz <itzabdiaziz@gmail.com>
2026-05-05 18:33:50 +0300

    add env

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Add token to every request
